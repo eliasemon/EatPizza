99af53879ce1cfa0e6170aef4b1892ce3883f62a
MH Murshed <arifsdarif@gmail.com>
2023-01-11 13:37:34 +0600

    counter in nav bar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,5 +1,6 @@
 import { FontAwesome } from "@expo/vector-icons";
-import { StyleSheet, View, TouchableOpacity , Text } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 import { useStoreState } from 'easy-peasy';
 import { COLORS } from '../constants/theme'
@@ -9,22 +10,51 @@ const NavBar = () => {
     const totalItemCount = useStoreState((state) => state.totalItemCount)
     const navigation = useNavigation();
 
+    const [activeButton, setActiveButton] = useState(0)
+
+    const navButtonList = [
+        {
+            id: 0,
+            icon: 'home',
+            link: "Home",
+        },
+        {
+            id: 1,
+            icon: 'user',
+            link: "Profile",
+        },
+        {
+            id: 2,
+            icon: 'shopping-cart',
+            link: "Checkout",
+            children: <View style={{ position: "relative" }}>
+                {totalItemCount > 0 && (
+                    <Text style={{ zIndex: 100, position: "absolute", top: -5, lef: 0, backgroundColor: 'red', paddingVertical: 1.5, paddingHorizontal: 5, borderRadius: 50, color: "white", fontSize: 12 }}>
+                        {totalItemCount}
+                    </Text>
+                )}
+            </View >,
+        },
+    ]
+
     return (
         <View style={styles.navigation}>
-            <TouchableOpacity style={{ flex: 1, alignItems: 'center' }} onPress={() => navigation.navigate("Home")}>
-                <FontAwesome name="home" size={28} color={COLORS.primary} />
-            </TouchableOpacity>
-            <TouchableOpacity style={{ flex: 1, alignItems: 'center' }} onPress={() => navigation.navigate('Profile')}>
-                <FontAwesome name="user" size={28} color={COLORS.primary} />
-            </TouchableOpacity>
-            <TouchableOpacity style={{ flex: 1, alignItems: 'center' }} onPress={() => navigation.navigate('Checkout')}>
-                <View style={{position : "relative"}}>
-                    {totalItemCount > 0 && (<Text style={{ zIndex: 100, position: "absolute" , top: -5, right : 0 , color : "yellow" }}>
-                    {totalItemCount}
-                    </Text>)}
-                    <FontAwesome name="shopping-cart" size={28} color={COLORS.primary} />
-                </View>
-            </TouchableOpacity>
+            {navButtonList.map((item) => <TouchableOpacity style={{ flex: 1, alignItems: 'center' }} onPress={() => { navigation.navigate(item.link); setActiveButton(item.id) }} key={item.id} >
+                {activeButton == item.id ?
+                    <View style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: 'rgba(0, 255, 0, 0.1)', paddingHorizontal: 10, paddingVertical: 5, borderRadius: 5 }}>
+                        <View>
+                            {item.children && item.children}
+                            < FontAwesome name={item.icon} size={28} color={COLORS.primary} />
+                        </View>
+                        <Text style={{ marginLeft: '10%', color: '#fff' }}>{item.link}</Text>
+                    </View>
+                    :
+                    <>
+                        {item.children && item.children}
+                        < FontAwesome name={item.icon} size={28} color={COLORS.primary} />
+                    </>
+                }
+            </TouchableOpacity>)}
         </View>
     );
 }
